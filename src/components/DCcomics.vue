14ba2a784e3cc41a1bfc6f8bf9f091e614031b18
GiovanniCalia <johnca@outlook.it>
2022-03-22 17:30:52 +0100

    Immagine messa sul browser con successo

--- src/components/DCcomics.vue
+++ src/components/DCcomics.vue
@@ -1,10 +1,16 @@
 <template>
 <header>
-    ciao
+    <img src="../../src/assets/img/dc-logo.png" alt="DC logo">
+    ciao {{ text }}
 </header>
 </template>
 <script>
 export default {
-  name: 'DCcomics'
+  name: 'DCcomics',
+  data () {
+    return {
+      text: 'ehi'
+    }
+  }
 }
 </script>
